e5a1272dd6339981232201c7dcc28c8b52f75d74
Owen Wholley <owenwholley@Dab-Lab-mobile.local>
2020-05-04 18:26:14 -0400

    first commit

---
+++ dla_bottom_seed.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from particle import Particle
+
+
+#------------------------------------------------------------------------------
+# Generate the particles and run them through the simulation
+axis_length = int(input("Please enter the desired length of the axes: "))
+number_particles = int(input("Please enter the desired number of particles: "))
+seed =  round(axis_length / 2)
+
+stuck_list = [
+    [seed, 0],
+]
+
+particles = [
+    Particle(axis_length, stuck_list) for i in range(0, number_particles, 1)
+]
+
+
+for particle in particles:
+    if particle.walk_particle() :
+        stuck_list.append([particle.x, particle.y],)
+
+print(stuck_list)
+
+
+#------------------------------------------------------------------------------
+# Plot
+
+axis = np.arange(0, axis_length, 1) # Set the axis length and the seed position
+
+for element in stuck_list:
+    plt.plot(element[0], element[1], 'ro')
+
+plt.plot(seed, 0, 'ko')                                     # Plot functions
+
+plt.xticks(np.arange(min(axis), max(axis)+1, 1), [])
+plt.yticks(np.arange(min(axis), max(axis)+1, 1), [])
+plt.grid()
+
+plt.show()
---
+++ particle.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Particle: #(Aggregate):
+
+    def __init__(self, axis_length, stuck_list):
+        # Aggregate.__init__(self, axis_length, number_particles)
+        self.axis_length = axis_length
+        self.stuck_list = stuck_list
+        self.seed = round(axis_length / 2)
+        self.x = np.random.randint(0, self.axis_length)
+        self.y = axis_length 
+        self.steps = 0
+
+    def displayParticlePosition(self):
+        print(f'Particle Position: [{self.x}, {self.y}] \n')
+
+    def take_step(self):
+        
+        # direction = 2
+        direction = np.random.randint(0,3)      # Randomly generate a number 0, 1, 2, or 3 to indicate movement up, right,
+                                                # down, or left, respectively
+        if direction == 0:          # up
+            self.y += 1
+        elif direction == 1:        # right
+            self.x += 1
+        elif direction == 2:        # down
+            self.y -= 1
+        else:                       # left
+            self.x -= 1
+        
+        self.steps += 1
+
+    def at_boundary(self):
+        # at boundary
+        if self.y < 0 or self.y > self.axis_length:
+            print(f'Whoops!! Particle left the lattice after {self.steps} steps!')
+            return True
+        elif self.x < 0 or self.x > self.axis_length:                       # Axis is precariously hardcoded ATM ****
+            print(self.y)
+            print(f'Whoops!! Particle left the lattice after {self.steps} steps!')
+            return True
+        # not at boundary
+        else:
+            # print(f'Still on the lattice after {self.steps} steps!')
+            return False
+
+    def is_frozen(self):
+
+        # stuck
+        if [self.x, self.y-1] in self.stuck_list or \
+           [self.x, self.y+1] in self.stuck_list or \
+           [self.x-1, self.y] in self.stuck_list or \
+           [self.x+1, self.y] in self.stuck_list:
+            print(f'STUCK!!!! Particle stuck after {self.steps} steps')
+            self.displayParticlePosition()
+            return True
+        # free
+        else:
+            # print(f'Still free after {self.steps} step(s)')
+            return False
+
+    def walk_particle(self):
+        while self.is_frozen() is False and self.at_boundary() is False:
+            self.take_step()
+            if self.is_frozen() is True:
+                return [self.x, self.y]
+        
+
+
